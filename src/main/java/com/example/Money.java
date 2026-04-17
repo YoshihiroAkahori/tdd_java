2a44eb7cf11a6a2075ba3089a9e758cc23d1331b
YoshihiroAkahori <akahori.yoshihiro.a5@daiichisankyo.co.jp>
2026-04-18 05:21:58 +0900

    4/17 night

--- src/main/java/com/example/Money.java
+++ src/main/java/com/example/Money.java
@@ -2,10 +2,33 @@ package com.example;
 
 class Money {
     protected int amount;
-
-
+    protected String currency;
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+    Money plus (Money addend) {
+        return new Money(amount + addend.amount, currency);
+    }
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+    public String toString() {
+        return amount + " " + currency;
+    }
+    String currency() {
+        return currency;
+    }
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount;
+        return amount == money.amount
+            && currency().equals(money.currency());
     }
+    static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+    static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
 }
